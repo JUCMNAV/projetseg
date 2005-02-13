3d35a1311f14f5cf18ae4a9195e8fec3af97610b
jpdaigle <jpdaigle@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-02-13 00:06:40 +0000

    Initial import with Eclipse - NEVER GONNA WORK. Salut flocon.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@9 1d5a7336-c65e-6947-a78c-d4236001c669

---
+++ seg.DemoViews/src/seg/DemoViews/views/UCMOutlineView.java
@@ -0,0 +1,73 @@
+/*
+ * Created on 12-Feb-2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package seg.DemoViews.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.part.*;
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.graphics.*;
+
+/**
+ * @author jpdaigle
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class UCMOutlineView extends ViewPart {
+
+	private Tree t;
+	
+	public UCMOutlineView() {
+		// ctor
+		super();
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		t = new Tree(parent, 0);
+		fillTreeItemsDummy(parent);
+	}
+
+	private void fillTreeItemsDummy(Composite parent) {
+		
+		TreeItem tiOutlineComponents = new TreeItem(t,0);
+		tiOutlineComponents.setText("Components");
+		
+		// it don't work :(
+		//tiOutlineComponents.setImage(new Image(parent.getDisplay(),"icons/fldr_obj.gif"));
+		
+
+		TreeItem tiOutlineResponsibilities = new TreeItem(t,0);
+		tiOutlineResponsibilities.setText("Responsibilities");
+
+		for(int i=0; i<5; i++) {
+			TreeItem tiInnerNodeComponent = new TreeItem(tiOutlineComponents,0);
+			tiInnerNodeComponent.setText("Component " + i);
+			
+			TreeItem tiInnerNodeResponsibility = new TreeItem(tiOutlineResponsibilities,0);
+			tiInnerNodeResponsibility.setText("Responsibility " + i);
+		}
+		
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchPart#setFocus()
+	 */
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		t.setFocus();
+	}
+
+}
