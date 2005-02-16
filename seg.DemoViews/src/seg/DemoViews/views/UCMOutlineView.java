3e66f91c534ad44b160f529dc6d3bd201437a397
jpdaigle <jpdaigle@1d5a7336-c65e-6947-a78c-d4236001c669>
2005-02-16 14:34:47 +0000

    got images mostly working
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@15 1d5a7336-c65e-6947-a78c-d4236001c669

--- seg.DemoViews/src/seg/DemoViews/views/UCMOutlineView.java
+++ seg.DemoViews/src/seg/DemoViews/views/UCMOutlineView.java
@@ -13,6 +13,7 @@ import org.eclipse.swt.*;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.graphics.*;
+import org.eclipse.jface.resource.ImageDescriptor;
 
 /**
  * @author jpdaigle
@@ -46,16 +47,32 @@ public class UCMOutlineView extends ViewPart {
 		
 		// it don't work :(
 		//tiOutlineComponents.setImage(new Image(parent.getDisplay(),"icons/fldr_obj.gif"));
-
+		ImageDescriptor idFolder = ImageDescriptor.createFromFile(seg.DemoViews.DemoViewsPlugin.class, "../../../icons/fldr_obj.gif");
+		ImageData data = idFolder.getImageData();
+		Image img = new Image(parent.getDisplay(),data);
+		
+		tiOutlineComponents.setImage(img);
+		
 		TreeItem tiOutlineResponsibilities = new TreeItem(t,0);
 		tiOutlineResponsibilities.setText("Responsibilities");
+		tiOutlineResponsibilities.setImage(img);
 
+		// load up other images - this is ugly.
+		// real version will use a cache of ImageDescriptors declared centrally for the plugin
+		ImageDescriptor idComponent = ImageDescriptor.createFromFile(seg.DemoViews.DemoViewsPlugin.class, "../../../icons/template_obj.gif");
+		Image imgComponent = idComponent.createImage();
+		
+		ImageDescriptor idResponsibility = ImageDescriptor.createFromFile(seg.DemoViews.DemoViewsPlugin.class, "../../../icons/delete_obj.gif");
+		Image imgResponsibility = idResponsibility.createImage();
+		
 		for(int i=0; i<5; i++) {
 			TreeItem tiInnerNodeComponent = new TreeItem(tiOutlineComponents,0);
 			tiInnerNodeComponent.setText("Component " + i);
+			tiInnerNodeComponent.setImage(imgComponent);
 			
 			TreeItem tiInnerNodeResponsibility = new TreeItem(tiOutlineResponsibilities,0);
 			tiInnerNodeResponsibility.setText("Responsibility " + i);
+			tiInnerNodeResponsibility.setImage(imgResponsibility);
 		}
 		
 	}
