5d69145df4a8cbe04ebf081152dfa4b4516d9a6b
iandritoiu <iandritoiu@1d5a7336-c65e-6947-a78c-d4236001c669>
2006-01-21 19:18:44 +0000

    Redesigned framework. Conversion process is now done through polymorphism.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@644 1d5a7336-c65e-6947-a78c-d4236001c669

---
+++ UCM2CSM/src/one2one/AbstractConverter.java
@@ -0,0 +1,16 @@
+package one2one;
+
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+import ucm.map.UCMmap;
+/**
+ * <!-- begin-user-doc -->
+ * It provides a convert method for all UCM to CSM Explicit Mappings.
+ * <!-- end-user-doc -->
+ * @see one2one 
+ * @generated
+ */
+public interface AbstractConverter {    
+    public void Convert(PrintStream ps);
+}
