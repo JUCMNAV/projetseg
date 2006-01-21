ec6bbf7ca5c46b542fbec9e61c573d7040097feb
iandritoiu <iandritoiu@1d5a7336-c65e-6947-a78c-d4236001c669>
2006-01-21 19:16:36 +0000

    Redesigned framework. Conversion process is now done through polymorphism.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@642 1d5a7336-c65e-6947-a78c-d4236001c669

---
+++ UCM2CSM/src/one2one/UCM2CSMPlugin.java
@@ -0,0 +1,54 @@
+package uCM2CSM;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class UCM2CSMPlugin extends AbstractUIPlugin {
+
+	//The shared instance.
+	private static UCM2CSMPlugin plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public UCM2CSMPlugin() {
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static UCM2CSMPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path.
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin("UCM2CSM", path);
+	}
+}
