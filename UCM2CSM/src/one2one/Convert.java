924a8dfa277b9b572c2f5efcc843f3d7d514f939
iandritoiu <iandritoiu@1d5a7336-c65e-6947-a78c-d4236001c669>
2006-01-27 00:35:18 +0000

    CSM-compliant version. Need to change plugin info!
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@660 1d5a7336-c65e-6947-a78c-d4236001c669

--- UCM2CSM/src/one2one/Convert.java
+++ UCM2CSM/src/one2one/Convert.java
@@ -5,45 +5,66 @@ import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Iterator;
 
-import org.eclipse.draw2d.IFigure;
-
-import seg.jUCMNav.extensionpoints.IUseCaseMapExport;
+import seg.jUCMNav.extensionpoints.IURNExport;
+import ucm.map.AndJoin;
+import ucm.map.OrJoin;
 import ucm.map.PathNode;
 import ucm.map.UCMmap;
+import urn.URNspec;
+import urncore.SpecificationDiagram;
+/**
+ * <!-- begin-user-doc -->
+ * Performs a 1-to-1 conversion on UCM components 
+ * <!-- end-user-doc -->
+ * @see one2one 
+ * @generated
+ */
 
-// Performs a 1-to-1 conversion on UCM components 
-public class Convert implements IUseCaseMapExport {
-    
+public class Convert implements IURNExport {
+	
+	// Converts object through polymorphism (dynamic binding)
     public void doConvert(AbstractConverter pn, PrintStream ps){
         pn.Convert(ps);
     }
-    // for every UCM component this method will generate its appropriate CSM representation
-    public void export(UCMmap map, FileOutputStream fos) throws InvocationTargetException {        
+
+	public void export(URNspec urn, FileOutputStream fos) throws InvocationTargetException {
         PrintStream ps = new PrintStream(fos); 
         // general CSM header and footer
         String CSM_header = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";       
         String CSM_footer = "</CSM>";
         ps.println(CSM_header);
-       
-        // parsing the component (CoreScenario) package
-        for (Iterator iter = map.getNodes().iterator(); iter.hasNext();) {
-            PathNode node = (PathNode) iter.next();          
-            if(node.getClass().getName()== "ucm.map.impl.OrJoinImpl"){
-               OrJoinConverter obj = new OrJoinConverter(node); 
-                doConvert(obj,ps);
-            }            
-        }
-        ps.println(CSM_footer);
-        ps.flush();       
-    }
-
-    public void export(IFigure map, FileOutputStream fos) throws InvocationTargetException {
-    }
-
-    public void export(UCMmap map, String path) throws InvocationTargetException {
-    }
-    
-    public void export(IFigure map, String path) throws InvocationTargetException {
-    }
+        
+        // parsing SpecDiagram
+        for (Iterator iter = urn.getUrndef().getSpecDiagrams().iterator(); iter.hasNext();) {
+			SpecificationDiagram diag = (SpecificationDiagram) iter.next();
+			if (diag instanceof UCMmap) {
+				UCMmap map = (UCMmap) diag;
+		        exportMap(map, ps);
+			}			
+		}
+       ps.println(CSM_footer);
+       ps.flush();       
+	}
+	
+	private void exportMap(UCMmap map, PrintStream ps) {
+		// parsing the map 
+		for (Iterator iter2 = map.getNodes().iterator(); iter2.hasNext();) {
+		    PathNode node = (PathNode) iter2.next();
+		    // if OrJoin UCM object is found, generate CSM representation
+		    if(node instanceof OrJoin){
+		       OrJoinConverter obj = new OrJoinConverter(node); 
+		       doConvert(obj,ps);
+		    }
+		    // if AndJoin UCM object is found, generate CSM representation
+		    if(node instanceof AndJoin){
+		       AndJoinConverter obj = new AndJoinConverter(node); 
+		       doConvert(obj,ps);
+		    }         
+		}
+	}
+	
+	public void export(URNspec urn, String filename) throws InvocationTargetException {
+		// TODO Auto-generated method stub		
+	}
 
 }
--- UCM2CSM/src/one2one/OrJoinConverter.java
+++ UCM2CSM/src/one2one/OrJoinConverter.java
@@ -12,19 +12,25 @@ import ucm.map.impl.OrJoinImpl;
  * @generated
  */
 public class OrJoinConverter extends OrJoinImpl implements AbstractConverter {
-
+	
+	// constructors
     public OrJoinConverter(PathNode pn){
        this.description = pn.getDescription();
        this.id = pn.getId();
        this.name = pn.getName();
     }
 
+    // prints XML representation of object to output file
     public void Convert(PrintStream ps){
-        String Object_header = "<CSM name=\"merge\" description=\"\" >";      
-        String Object_attributes = "<merge id=\"" + getId() + "\"" + " name=\""+ getClass().getName()+"\" />";        
+        String Object_header = "<CSM name=\"merge\" description=\"\">";      
+        String Object_attributes = "<merge id=\"" + getId() + "\"/>";
+        String Open_scenario_tag = "<Scenario id=\"" + getId() + "\"" + " name=\""+ getName()+"\"description= \"" + getDescription()+ "/>";
+        String Close_scenario_tag = "</Scenario>";
         // output to file
         ps.println("    " + Object_header);
+        ps.println("        " + Open_scenario_tag);
         ps.println("        " + Object_attributes);
+        ps.println("        " + Close_scenario_tag);
         ps.flush();                    
     }
 }
