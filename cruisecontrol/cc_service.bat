d65b67f192bf50b38e0ae33a8855b41738466c44
jkealey <jkealey@1d5a7336-c65e-6947-a78c-d4236001c669>
2006-02-02 18:54:56 +0000

    automatically running map_ccbuilds.vbs when cruisecontrol is launched.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@705 1d5a7336-c65e-6947-a78c-d4236001c669

--- cruisecontrol/cc_service.bat
+++ cruisecontrol/cc_service.bat
@@ -1,3 +1,10 @@
 @echo off
 cd c:\work\cruisecontrol\
+
+REM this script maps the network drive required for CruiseControl
+map_ccbuilds.vbs
+
+REM wait 2 seconds just in case the mapping takes a bit longer
+sleep 2
+
 cruisecontrol.bat
