6d6baa553fac46ec8a34d69878be22639f349f17
iandritoiu <iandritoiu@1d5a7336-c65e-6947-a78c-d4236001c669>
2006-02-16 03:03:13 +0000

    Component Converter class now ready. Still need to modify the attribute names according to the new schema.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@733 1d5a7336-c65e-6947-a78c-d4236001c669

--- UCM2CSM/src/one2one/ComponentConverter.java
+++ UCM2CSM/src/one2one/ComponentConverter.java
@@ -1,7 +1,7 @@
 package one2one;
-
-import java.awt.List;
 import java.io.PrintStream;
+import java.util.Iterator;
+import java.util.*;
 
 import ucm.map.ComponentRef;
 import urncore.ComponentElement;
@@ -19,100 +19,90 @@ public class ComponentConverter implements AbstractConverter {
 	private ComponentRef compRef;
     private ComponentRef parentCompRef;
     private ComponentRef childrenCompRef;
-    private List childrenList = new List();    
     
     // component definitions
 	private ComponentElement compDef;
 	private ComponentElement parentCompDef;
     private ComponentElement childrenCompDef;
     
-    // other variables
-    private boolean hasParent; 	
-	private boolean hasChildren;
-	
+    // other variables    
+    private String children = new String();
+    private String parent = new String();
+    
 	// constructors
 	public ComponentConverter(ComponentRef compRef) {
-		hasParent = true;
-		hasChildren = true;
 		this.compRef = compRef;
 		this.compDef = (ComponentElement) compRef.getContDef();
+        
 		// initialize parent only if a reference to the parent component exists
 		if (((ComponentRef) compRef.getParent()) != null){
 			this.parentCompRef = (ComponentRef) compRef.getParent();			 
 			// if (this.parentCompRef != null)
 			this.parentCompDef = (ComponentElement) this.parentCompRef.getContDef();
+            parent += "c" + this.parentCompDef.getId();
 		}
 		else{
-			hasParent = false;
-		}							
-		// initialize children only if a reference to the child component exists
-        //this.childrenCompRef = (ComponentRef) compRef.getChildren();
-        // implement a LIST ??
-   /*     this.childrenList = (List) compRef.getChildren();      
-        System.out.println("In ComponentConverter about to retreive data ");
-	    // if (((ComponentRef) compRef.getChildren()!= null)){
-        if (!childrenCompRef.getChildren().isEmpty()){
-			// if (this.childrenCompRef != null)
-			this.childrenCompDef = (ComponentElement) this.childrenCompRef.getContDef();
+            parent += " ";
+            System.out.println("Parent String: " + children);			
 		}
-		else {
-			hasChildren = false;
-		}
-     */ 												
+        
+		// retrieve children
+        int j = 0;   
+       
+        for (Iterator iter = compRef.getChildren().listIterator(); iter.hasNext();) {               
+            this.childrenCompRef = (ComponentRef) iter.next(); 
+            this.childrenCompDef = (ComponentElement) this.childrenCompRef.getContDef();                 
+            j++;
+            System.out.println("Child #" + j + ": " + this.childrenCompDef.getId());
+            children += "c" + this.childrenCompDef.getId() + " ";
+            System.out.println("Children String: " + children);
+            //hasChildren = true;              
+       }
+      												
 	}
 
 	// prints XML representation of object to output file
 	public void Convert(PrintStream ps) {
-		// for debug
-		System.out.println("In ComponentConverter about to retreive data ");
-		System.out.println("Get id = " + compDef.getId());
-		System.out.println("Get name = " + compDef.getName());
-		if (hasParent)
-			System.out.println("Get parent = " + parentCompDef.getId());
-//		if (hasChildren)
-//			System.out.println("Get children = " + childrenCompDef.getId());
-      
 
 		// object attributes
-/*			
+			
 		 String comp_attributes = "<Component id=\"" + "c" + compDef.getId() + "\"" + " " +
 		 						  "name=\"" + compDef.getName() + "\"" + " " +
 		 						  "host=\"" + " " + "\"" + " " + "\" />";
 		  
 		 String comp_attributes_sub = "<Component id=\"" + "c" + compDef.getId() + "\"" + " " +
 		 							  "name=\"" + compDef.getName()+ "\"" + " " +
-		 							  "host=\"" + " " + "\"" + " " + "sub=\"" + childrenCompDef.getId() + "\"/>";
-		 
-		 String comp_attributes_parent = "<Component id=\"" + "c" + compDef.getId() + "\"" + " " +
-		 								 "name=\"" + compDef.getName() + "\"" + " " +
-		 								 "host=\"" + " " + "\"" + " " +
-		 								 "parent=\"" + parentCompDef.getId() + "\" />";
-		  
+		 							  "host=\"" + " " + "\"" + " " + "sub=\"" + children + "\"/>";
+       
+         String comp_attributes_parent = "<Component id=\"" + "c" + compDef.getId() + "\"" + " " +
+                                         "name=\"" + compDef.getName() + "\"" + " " +
+                                         "host=\"" + " " + "\"" + " " +
+                                         "parent=\"" + parent + "\" />";
+  		  
 		 String comp_attributes_sub_parent = "<Component id=\"" + "c" + compDef.getId() + "\"" + " " +
 		 									 "name=\"" + compDef.getName() + "\"" + " " +
 		 									 "host=\"" + " " + "\"" + " " +
-		 									 "sub=\"" + childrenCompDef.getId() + "\"" + " " +
-		 									 "parent=\"" + parentCompDef.getId() + "\" />";		
-*/		 
+		 									 "sub=\"" + children + "\"" + " " +
+		 									 "parent=\"" + parent + "\" />";		
+	 
 		// determine if component has parents or children or both and output to file
-/*		 
-		 if(parentCompDef.getId() != null){
-			 if(childrenCompDef.getId() != null){
-				 ps.println(" " + comp_attributes_sub_parent);
+ 
+		 if(parent != " "){
+			 if(children != " "){
+				 ps.println("            " + comp_attributes_sub_parent);
 			 }
 			 else{
-				 ps.println(" " + comp_attributes_parent);
+				 ps.println("            " + comp_attributes_parent);
 			 }
 		 }
 		 else {
-			 if(childrenCompDef.getId() != null){
-				 ps.println(" " + comp_attributes_sub);
+             if(children != " "){
+				 ps.println("            " + comp_attributes_sub);
 			 }
 			 else{
-				 ps.println(" " + comp_attributes);
+				 ps.println("            " + comp_attributes);
 			 }
-		 }
-*/		
+		 }	
 		ps.flush();
 
 	}
