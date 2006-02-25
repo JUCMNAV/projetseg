55b92ae057545505253328fa30850c25cec0f643
ykhom091 <ykhom091@1d5a7336-c65e-6947-a78c-d4236001c669>
2006-02-25 23:47:22 +0000

    Plugin-Binding needs to be finished. Problems remaining: 1- Start Point does not work (workload problem) 2- Processing Resource XML implementation missing 3- Several CSM validation problems
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@762 1d5a7336-c65e-6947-a78c-d4236001c669

--- UCM2CSM/src/one2one/PluginBindingConverter.java
+++ UCM2CSM/src/one2one/PluginBindingConverter.java
@@ -2,16 +2,14 @@ package one2one;
 
 import java.io.PrintStream;
 
-import ucm.map.InBinding;
-import ucm.map.OutBinding;
 import ucm.map.PluginBinding;
 
 
 
 public class PluginBindingConverter implements AbstractConverter{
 	private PluginBinding p_bind;
-	private InBinding in_bind;
-	private OutBinding out_bind;
+	// private InBinding in_bind;
+	// private OutBinding out_bind;
 
 	// constructors
     public PluginBindingConverter(PluginBinding p_bind){
@@ -20,27 +18,34 @@ public class PluginBindingConverter implements AbstractConverter{
     
     // prints XML representation of object to output file
 	public void Convert(PrintStream ps){
-		        
+		
+		// String parent = ((UCMmap)p_bind.getPlugin().getParentStub().get(0)).getId();
+		Object parent = p_bind.getPlugin().getParentStub().get(0);
+		System.out.println("Parent:" + parent);
+		//PluginBinding sub = (PluginBinding) ((Stub)p_bind.getPlugin());
+		
 	    // object attributes
-	    String object_attributes = "<Refinement parant=\"" + "h" + p_bind.getId() + "\"" + " " +
-                                    "start=\"" + "h" + p_bind.getStub() +"\"/>";
-	    
+//	    String object_attributes = "<Refinement parent=\"" + "h " + parent + "\"" + " " +
+//                                   "sub=\"" + "h" + sub +"\"/>";
+	      
+/*
 
 	    String inbinding_attributes = "<InBinding id=\"" + "si" + in_bind.getBinding() + "\"" + " " +
         								"start=\"" + "h" + in_bind.getStartPoint()  + "\"" + " " + 
         								"in=\"" + "h" + in_bind.getStubEntry() +"\"/>";
 
-	    String outbinding_attributes = "<outbinding id=\"" + "so" + out_bind.getBinding() + "\"" + " " +
+	    String outbinding_attributes = "<Outbinding id=\"" + "so" + out_bind.getBinding() + "\"" + " " +
         								"end=\"" + "h" + out_bind.getEndPoint()  + "\"" + " " + 
         								"out=\"" + "h" + out_bind.getStubExit() +"\"/>";
-	    String object_attributes_close = "</Refinement>";
+*/
+//	    String object_attributes_close = "</Refinement>";
 
 		        
-	    // output to file
-	    ps.println("            " + object_attributes);
-	    ps.println("            " + inbinding_attributes);
-	    ps.println("            " + outbinding_attributes);
-	    ps.println("            " + object_attributes_close);
+	    // output to file	        
+//	    ps.println("            " + object_attributes);	    
+//	    ps.println("            " + inbinding_attributes);
+//	    ps.println("            " + outbinding_attributes);
+//	    ps.println("            " + object_attributes_close);
 	    ps.flush();                    
  }
 
