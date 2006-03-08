cae39eab17c392671e4600b637e1bfa40992e3d7
iandritoiu <iandritoiu@1d5a7336-c65e-6947-a78c-d4236001c669>
2006-03-08 04:03:14 +0000

    Further work on ResourceAcquire.
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@795 1d5a7336-c65e-6947-a78c-d4236001c669

--- UCM2CSM/src/implicit/ResourceAcquisition.java
+++ UCM2CSM/src/implicit/ResourceAcquisition.java
@@ -1,6 +1,7 @@
 package implicit;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Stack;
 
 import ucm.map.ComponentRef;
@@ -8,64 +9,89 @@ import ucm.map.PathNode;
 import ucm.map.UCMmap;
 
 public class ResourceAcquisition {
-    
-          
+              
     // other variables    
-    private String prev_edge_comp;
-    
+    // private ComponentRef prev_edge_comp_ref;
+       
     // constructor 
-    public ResourceAcquisition(){
-        
+    public ResourceAcquisition(){        
     }
     
     // adds all hyperedge parent components to stack
     public void findParentsRA(ComponentRef component_ref, Stack edge_stack){
         // The top of the stack is the outermost component
+        System.out.println("componentRef: " + (ComponentRef) component_ref);
         ComponentRef parent_ref = (ComponentRef) component_ref.getParent();
         if (parent_ref != null){
             edge_stack.push(parent_ref);
             findParentsRA(parent_ref, edge_stack);
         }                
     }
-    
+  
     // Resource Acquire algorithm
     public void acquireResource(PathNode curr_edge, UCMmap map){
-        // local variables
-        ArrayList prev_edge_list = new ArrayList(1000);
-        Stack curr_edge_stack = new Stack ();        
+        // local variables          
+        ComponentRef prev_edge_comp_ref;
+        Stack curr_edge_stack = new Stack ();
+        // populate local prev_edge_list
+        List prev_edge_list = map.getNodes().subList(0,map.getNodes().size()-1);
+        // System.out.println("Previous edges: " + map.getNodes().subList(0,map.getNodes().size()-1));
+        ComponentRef curr_edge_comp_ref = (ComponentRef) curr_edge.getContRef();
         
         // current edge is inside component 
-        if (curr_edge.getContRef()!= null ){
-            // find the parent component of current edge
-            ComponentRef curr_edge_comp_ref = (ComponentRef) curr_edge.getContRef();                    
+        if (curr_edge_comp_ref != null ){
+            // find the parent component of current edge                                        
             curr_edge_stack.push(curr_edge_comp_ref);
             findParentsRA(curr_edge_comp_ref, curr_edge_stack);
             
-            // debug - curr_edge_stack contents
-            for (int j = 0; j < curr_edge_stack.size(); j ++){ 
-                System.out.println("curr_edge_stack " + j + " " + curr_edge_stack.get(j));
-            }            
-           
- /*                       
             if (!prev_edge_list.isEmpty()){
-                for (int i = 0 ; i < prev_edge_list.size(); i++){ // Iterator list_iterator = prev_edge_list.iterator();list_iterator.hasNext();){
+                for (int j = 0; j < prev_edge_list.size(); j++){                      
                     // Previous edge must be in a different component
-                    prev_edge_comp = ((ComponentElement)((PathNode)prev_edge_list.get(i-1))).getId();
-                    if (prev_edge_comp != curr_edge_comp){
-                        //find which parents of curr_edge are not included in those prev_edge
-                        // difference, keeps outside components
+                    PathNode prev_edge = (PathNode) prev_edge_list.get(j);                                        
+                    prev_edge_comp_ref = (ComponentRef) prev_edge.getContRef(); //PROBLEM!!!
+
+                    if (prev_edge_comp_ref != curr_edge_comp_ref){
+                        
+                        // Find which parents of curr_edge are not included in those prev_edge
+                        Stack prev_edge_stack = new Stack ();
+                        prev_edge_stack.push(prev_edge_comp_ref);
+                        findParentsRA(prev_edge_comp_ref, prev_edge_stack);
+                        
+                        // Difference, keeps outside components
+                        Stack outside_comp_stack = new Stack ();
+                        
+                        for (int i=0; i < prev_edge_stack.size(); i++){
+                            for (int k=0; k < curr_edge_stack.size(); k++){
+                                if (prev_edge_stack.get(i) == curr_edge_stack.get(k)){
+                                    outside_comp_stack.push(curr_edge_stack.get(k));
+                                }
+                            }    
+                        }
+                        // debug - prev_edge_stack contents
+                        for (int a = 0; a < prev_edge_stack.size(); a ++){ 
+                            System.out.println("prev_edge_stack " + a + " " + prev_edge_stack.get(a));
+                        }
+                        
+                        // debug - curr_edge_stack contents
+                        for (int b = 0; b < curr_edge_stack.size(); b ++){ 
+                             System.out.println("curr_edge_stack " + b + " " + curr_edge_stack.get(b));
+                        }
+                        // debug - outside_comp_stack contents
+                        for (int c = 0; c < outside_comp_stack.size(); c ++){ 
+                             System.out.println("outside_comp_stack " + c + " " + outside_comp_stack.get(c));
+                        }    
                         // Acquire the components of the parents
                     }
+                    
                 }                
-            }            
-        }
-*/
+            }                            
         }
         else {
             // Must be a start point, acquire the components
             System.out.println("acquireResource:  This is a StartPoint");
         }
     }
+    
     
     // prints XML representation of Resource Acquire element
     public void acquireComp(){
