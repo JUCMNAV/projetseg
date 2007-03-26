49345e322ea034e3acd537bc80ccb35898c5ee55
jack <jack@1d5a7336-c65e-6947-a78c-d4236001c669>
2007-03-26 22:22:09 +0000

    Producing <ExternalDemand ...>
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@1172 1d5a7336-c65e-6947-a78c-d4236001c669

--- UCM2CSM/src/one2one/ResponsibilityRefConverter.java
+++ UCM2CSM/src/one2one/ResponsibilityRefConverter.java
@@ -2,8 +2,10 @@ package one2one;
 
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import ucm.map.RespRef;
+import ucm.performance.Demand;
 
 /**
  * <!-- begin-user-doc --> Creates the CSM representation(Step) of the Responsibility object <!-- end-user-doc -->
@@ -29,13 +31,28 @@ public class ResponsibilityRefConverter implements AbstractConverter {
                 + target.toString().subSequence(1, (target.toString().length() - 1)) + "\"";
 
         ps.print("            " + mandatory_attribute);
-        String closing_attribute = "/>";
-
+        
         // optional attributes
         sa.OptionalAttributes(resp, ps);
-
-        // output to file
-        ps.println(closing_attribute);
+        
+        if (resp.getRespDef().getDemands().size() == 0) {
+            String closing_attribute = "/>";
+            // output to file
+            ps.println(closing_attribute);    
+        } else {
+            String closing_attribute1 = ">";
+            // output to file
+            ps.println(closing_attribute1);
+            for (Iterator demands = resp.getRespDef().getDemands().iterator(); demands.hasNext();) {
+		Demand demand = (Demand) demands.next();
+		String demand_line = "                  <ExternalDemand demand=\"" + demand.getQuantity() + "\" extOp=\"e" + demand.getResource().getId() + "\"/>";
+		ps.println(demand_line);
+	    }
+            String closing_attribute2 = "            </Step>";
+            // output to file
+            ps.println(closing_attribute2);
+        }
+        
         ps.flush();
     }
 }
