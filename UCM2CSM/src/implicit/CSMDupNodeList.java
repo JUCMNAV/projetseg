d3a6281d97891948ca9d893f15238fbf3b9c6472
jack <jack@1d5a7336-c65e-6947-a78c-d4236001c669>
2007-04-26 15:55:51 +0000

    Converted from ArrayList to [].
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@1245 1d5a7336-c65e-6947-a78c-d4236001c669

--- UCM2CSM/src/implicit/CSMDupNodeList.java
+++ UCM2CSM/src/implicit/CSMDupNodeList.java
@@ -1,6 +1,5 @@
 package implicit;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 
 import ucm.map.PathNode;
@@ -10,16 +9,19 @@ import ucm.map.UCMmap;
  * <!-- begin-user-doc --> Creates a list of CSMDupNodes, that is meant to replicate the UCM map <!-- end-user-doc -->
  * 
  * @see implicit
- * @generated
  */
 public class CSMDupNodeList {
     // will contain CSMDupNodes (which in turn may point to a PathNode or an RA node)
-    ArrayList nodeList = new ArrayList(1000);
+    // was: ArrayList nodeList = new ArrayList(1000);
+    // which made the content of the list unchangeable.
+    // TODO:  manage size limitation.
+    private CSMDupNode[] nodeList = new CSMDupNode[1000];
+    private int length = 0;
 
     // create list of PathNodes
     public void DuplicateHyperEdges(UCMmap map) {
         for (Iterator iter = map.getNodes().iterator(); iter.hasNext();) {
-            nodeList.add(new CSMDupNode((PathNode) iter.next()));
+            this.nodeList[this.length++] = new CSMDupNode((PathNode) iter.next());
         }
     }
 
@@ -27,48 +29,48 @@ public class CSMDupNodeList {
 
     // size of path list
     public int size() {
-        return nodeList.size();
+        return this.length;
     }
 
     // get node
     public CSMDupNode get(int i) {
-        return (CSMDupNode) nodeList.get(i);
+        return this.nodeList[i];
     }
 
     // add node at the end of list
     public void add(CSMDupNode node) {
-        nodeList.add(nodeList.size(), node);
+        this.nodeList[this.length++] = node;
     }
 
     // add node at a specific point in map
     public void add(int position, CSMDupNode node) {
-        nodeList.add(position, node);
+        this.nodeList[position] = node;
     }
 
     // checks if list is empty
     public boolean isEmpty() {
-        return nodeList.isEmpty();
+        return this.length == 0;
     }
 
     // return a PathNode from the Duplicate Graph
     public PathNode getListNode(int i) {
-        return ((CSMDupNode) (nodeList.get(i))).getNode();
+        return this.nodeList[i].getNode();
     }
 
     // for debug - prints ids of all elements in list
     public void printDupList() {
         System.out.println("----------Printing duplicate Node List-------");
-        System.out.println("List size: " + nodeList.size());
+        System.out.println("List size: " + this.length);
         String typeName;
-        for (int i = 0; i < nodeList.size(); i++) {
-            int type = ((CSMDupNode) (nodeList.get(i))).getType();
-            typeName = " (" + ((CSMDupNode) (nodeList.get(i))).getTypeString() + ")";
+        for (int i = 0; i < this.length; i++) {
+            int type = this.nodeList[i].getType();
+            typeName = " (" + this.nodeList[i].getTypeString() + ")";
             if (type == CSMDupNode.RA || type == CSMDupNode.RR || type == CSMDupNode.CSMEMPTY) {
-                String id = ((CSMDupNode) (nodeList.get(i))).getId();
+                String id = this.nodeList[i].getId();
                 System.out.println("Index " + i + " id: " + id + typeName);
             } else {
-                System.out.println("Node : " + ((CSMDupNode) (nodeList.get(i))).getNode());
-                String node_id = ((CSMDupNode) (nodeList.get(i))).getId();
+                System.out.println("Node : " + this.nodeList[i].getNode());
+                String node_id = this.nodeList[i].getId();
                 System.out.println("Index " + i + " id: " + node_id + typeName);
             }
         }
@@ -76,30 +78,46 @@ public class CSMDupNodeList {
 
     // returns the position of the node in the list of dup nodes
     public int getNodeIndex(PathNode node) {
-        for (int i = 0; i < nodeList.size(); i++) {
-            if (((CSMDupNode) (nodeList.get(i))).getNode() == node)
+        for (int i = 0; i < this.length; i++) {
+            if (this.nodeList[i].getNode() == node)
                 return i;
         }
         return -1; // not found
     }
 
-    // removes a node from the node list
+    // removes a node from the node list (and just one)
     public void remove(CSMDupNode node) {
-        for (int i = 0; i < nodeList.size(); i++) {
-            if (((CSMDupNode) (nodeList.get(i))).getNode() != null) {
-                if (((CSMDupNode) (nodeList.get(i))).getNode().getId() == node.getId()) {
-                    nodeList.remove(i);
+	boolean found = false;
+        for (int i = 0; i < this.length; i++) {
+            if (this.nodeList[i].getNode() != null) {
+                if (this.nodeList[i].getNode().getId() == node.getId()) {
+                    found = true;
+                    // this.nodeList.remove(i);
                 } // if
+                if (found && ((i+1) < this.length)) {
+                    this.nodeList[i] = this.nodeList[i+1];
+                }
             } // if
         } // for
+        if (found) {
+            this.length--;
+        }
     } // method
 
     public void remove(PathNode node) {
-        for (int i = 0; i < nodeList.size(); i++) {
-            if (((CSMDupNode) (nodeList.get(i))).getNode() == node) {
-                nodeList.remove(i);
+	boolean found = false;
+        for (int i = 0; i < this.length; i++) {
+            if (this.nodeList[i].getNode() == node) {
+                found = true;
+                // this.nodeList.remove(i);
             } // if
+            if (found && ((i+1) < this.length)) {
+                this.nodeList[i] = this.nodeList[i+1];
+            }
         } // for
+        if (found) {
+            this.length--;
+        }
     } // method
 
     /**
@@ -148,9 +166,6 @@ public class CSMDupNodeList {
      */
     public void retype(CSMDupNode oldNode, int type) {
 	int nodeIndex = getNodeIndex(oldNode.getNode());
-	CSMDupNode newNode = (CSMDupNode) nodeList.get(nodeIndex);
-	nodeList.remove(nodeIndex);
-	newNode.setType(type);
-	nodeList.add(nodeIndex, newNode);
+	this.nodeList[nodeIndex].setType(type);
     }
 }
