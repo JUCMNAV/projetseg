38d0623df258e8e9751cd2b20329cb148fa90c4b
slebl89 <slebl89@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-01-19 00:29:28 +0000

    Improve integration tests
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@1995 1d5a7336-c65e-6947-a78c-d4236001c669

--- aoUrnToRamWorkspace/javaExternCall/src/aoUrnToRam/javaExternalCall/File.java
+++ aoUrnToRamWorkspace/javaExternCall/src/aoUrnToRam/javaExternalCall/File.java
@@ -1,13 +1,17 @@
 package aoUrnToRam.javaExternalCall;
 
+import java.net.URI;
+
 import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
 import fr.irisa.triskell.kermeta.runtime.basetypes.String;
 
 public class File {
-	public static void removeDirectory(RuntimeObject directory) {
+	public static void removeDirectory_DirectoryUri(RuntimeObject directoryUri) {
+		
 		try{
-			java.io.File directoryAsFile = new java.io.File(String.getValue(directory));
-			FileImpl.removeDirectory(directoryAsFile);
+			URI directoryAsUri = new URI(String.getValue(directoryUri));
+			java.io.File directoryAsFile = new java.io.File(directoryAsUri);
+			DirectoryRemover.removeDirectory(directoryAsFile);
 		}
 		catch(Exception ex){
 			Logger.log("Error:"+ ex.toString());
