ff79bbbc7834c93e36a99d31a5fe83e57cdefaf8
slebl89 <slebl89@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-02-06 17:03:35 +0000

    Remove useless commented code
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2080 1d5a7336-c65e-6947-a78c-d4236001c669

--- aoUrnToRamWorkspace/javaExternCall/src/aoUrnToRam/javaExternalCall/CommandLineImpl.java
+++ aoUrnToRamWorkspace/javaExternCall/src/aoUrnToRam/javaExternalCall/CommandLineImpl.java
@@ -18,37 +18,4 @@ public class CommandLineImpl {
 		//return ReadInputStream(new BufferedInputStream(p.getInputStream()));
 		return "noOutput";
 	}
-	
-	/*
-	public static String exe(String command,String input) throws IOException{
-		Process p = Runtime.getRuntime().exec(command);
-		WriteOutputStream(p.getOutputStream(),input);
-		//return ReadInputStream(new BufferedInputStream(p.getInputStream()));
-		return "noOutput";
-	}
-
-
-	private static void WriteOutputStream(OutputStream ostream, String value) throws IOException{
-		ostream.write(value.getBytes());
-		ostream.close();
-	}
-	
-
-	private static String ReadInputStream(InputStream is) throws IOException{
-		String s = ""; //$NON-NLS-1$
-        StringBuffer builder = new StringBuffer();
-        if (is != null) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-
-            try {
-                while ((s = reader.readLine()) != null)
-                    builder.append(s + "\n"); //$NON-NLS-1$
-            } finally 
-            {
-            		reader.close();
-            }
-        }
-
-        return builder.toString();
-	}*/
 }
