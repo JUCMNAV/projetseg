77674d64138a81c0a24ca1240df5c560e4b76f05
slebl89 <slebl89@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-02-16 18:08:07 +0000

    WorkflowInstantiator is abstract
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2119 1d5a7336-c65e-6947-a78c-d4236001c669

--- aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/reactiveworkflow/WorkflowInstantiator.java
+++ aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/reactiveworkflow/WorkflowInstantiator.java
@@ -2,8 +2,10 @@ package ram.reactiveworkflow;
 
 import ram.workflow.Condition;
 
-public class WorkflowInstantiator{
+public abstract class WorkflowInstantiator{
 	protected CustomizableNode createCustomizableNode(String fullyQualifiedClassName){return null;}	
 	protected Condition createCondition(String fullyQualifiedClassName){return null;}
+	
+	public abstract void link();
 
 }
