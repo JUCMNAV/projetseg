be2938d5a44d715d07d1682b81f04d81a1f94079
slebl89 <slebl89@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-02-18 21:06:01 +0000

    Feature Completed: FeaWorkflowInstantiationWithStubs
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2137 1d5a7336-c65e-6947-a78c-d4236001c669

--- aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/workflow/ConditionalExecutionNode.java
+++ aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/workflow/ConditionalExecutionNode.java
@@ -2,4 +2,5 @@ package ram.workflow;
 
 public class ConditionalExecutionNode extends WorkflowNode {
 	public void addNextNode(String outPathname, WorkflowNode n, Condition c){}
+	public void addNextNode(String outPathname, WorkflowNode n, String inPathName, Condition c){}
 }
--- aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/workflow/WorkflowNode.java
+++ aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/workflow/WorkflowNode.java
@@ -2,4 +2,7 @@ package ram.workflow;
 
 public class WorkflowNode {
 	public void addNextNode(WorkflowNode nextNode){}
+	public void addNextNode(String outPathname, WorkflowNode n){}
+	public void addNextNode(WorkflowNode n, String inPathName){}
+	public void addNextNode(String outPathname, WorkflowNode n, String inPathName){}
 }
