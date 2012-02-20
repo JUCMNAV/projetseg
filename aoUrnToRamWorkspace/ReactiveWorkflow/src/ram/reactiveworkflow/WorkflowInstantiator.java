bb22516cf2c060cfc861fa42765ee67dca50e56a
slebl89 <slebl89@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-02-20 20:02:27 +0000

    Feature Completed: FeaSystemInstantiation
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2146 1d5a7336-c65e-6947-a78c-d4236001c669

--- aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/reactiveworkflow/WorkflowInstantiator.java
+++ aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/reactiveworkflow/WorkflowInstantiator.java
@@ -3,8 +3,13 @@ package ram.reactiveworkflow;
 import ram.workflow.Condition;
 
 public abstract class WorkflowInstantiator{
-	protected CustomizableNode createCustomizableNode(String fullyQualifiedClassName){return null;}	
-	protected Condition createCondition(String fullyQualifiedClassName){return null;}
+	protected CustomizableNode createCustomizableNode(String fullyQualifiedClassName){
+		return new CustomizableNode();//Fake
+	}	
+	
+	protected Condition createCondition(String fullyQualifiedClassName){
+		return new Condition();//Fake
+	}
 	
 	public abstract void link();
 
