766eac5da5d73d481e53d3a7306a795aaf26f1df
slebl89 <slebl89@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-03-26 15:22:25 +0000

    Feature Completed: FeaMakeCustomizableNodeResponsibleForOrFork
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2367 1d5a7336-c65e-6947-a78c-d4236001c669

--- aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/workflow/ConditionalExecutionNode.java
+++ aoUrnToRamWorkspace/ReactiveWorkflow/src/ram/workflow/ConditionalExecutionNode.java
@@ -1,6 +1,3 @@
 package ram.workflow;
 
-public class ConditionalExecutionNode extends WorkflowNode {
-	public void addNextNode(String outPathname, WorkflowNode n, Condition c){}
-	public void addNextNode(String outPathname, WorkflowNode n, String inPathName, Condition c){}
-}
+public class ConditionalExecutionNode extends WorkflowNode {}
