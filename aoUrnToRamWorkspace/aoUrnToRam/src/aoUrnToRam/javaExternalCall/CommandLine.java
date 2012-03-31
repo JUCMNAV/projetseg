79b29d9415f3a6f84d98aad767dff3294fd34c69
slebl89 <slebl89@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-03-31 14:07:47 +0000

    customizedLabel
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2412 1d5a7336-c65e-6947-a78c-d4236001c669

--- aoUrnToRamWorkspace/aoUrnToRam/src/aoUrnToRam/javaExternalCall/CommandLine.java
+++ aoUrnToRamWorkspace/aoUrnToRam/src/aoUrnToRam/javaExternalCall/CommandLine.java
@@ -6,8 +6,13 @@ import fr.irisa.triskell.kermeta.runtime.basetypes.String;
 public class CommandLine {
 	public static RuntimeObject exe(RuntimeObject command,RuntimeObject input){
 		try{
+			Logger.log("Info: command:"+ String.getValue(command));
+			Logger.log("Info: input:"+ String.getValue(input));
+			
 			java.lang.String result=CommandLineImpl.exe(String.getValue(command),String.getValue(input));
+			Logger.log("Info: done:"+result);
 			return String.create(result,command.getFactory());
+			
 		}
 		catch(Exception ex){
 			Logger.log("Error:"+ ex.toString());
