fb0dd13cc87d8c9509e7d4b9f91d59a535dd3e3a
slebl89 <slebl89@1d5a7336-c65e-6947-a78c-d4236001c669>
2012-05-08 20:10:26 +0000

    Feature Completed: FeaConvertSpaceToUnderscore
    git-svn-id: svn://cserg0.site.uottawa.ca/projetseg/trunk@2533 1d5a7336-c65e-6947-a78c-d4236001c669

--- aoUrnToRamWorkspace/aoUrnToRam/src/aoUrnToRam/javaExternalCall/StringUtil.java
+++ aoUrnToRamWorkspace/aoUrnToRam/src/aoUrnToRam/javaExternalCall/StringUtil.java
@@ -18,4 +18,33 @@ public class StringUtil {
 			return String.create(null,str.getFactory());
 		}
 	}
+	
+	public static RuntimeObject replaceAll(RuntimeObject str,RuntimeObject regex, RuntimeObject replacement){
+		try{
+			java.lang.String result=StringUtilImpl.replaceAll(
+					String.getValue(str),
+					String.getValue(regex),
+					String.getValue(replacement)
+			);
+			return String.create(result,str.getFactory());
+		}
+		catch(Exception ex){
+			Logger.logError(ex);
+			return String.create(null,str.getFactory());
+		}
+	}
+
+	
+	public static RuntimeObject trim(RuntimeObject str){
+		try{
+			java.lang.String result=StringUtilImpl.trim(
+					String.getValue(str)
+			);
+			return String.create(result,str.getFactory());
+		}
+		catch(Exception ex){
+			Logger.logError(ex);
+			return String.create(null,str.getFactory());
+		}
+	}
 }
--- aoUrnToRamWorkspace/aoUrnToRam/src/aoUrnToRam/javaExternalCall/StringUtilImpl.java
+++ aoUrnToRamWorkspace/aoUrnToRam/src/aoUrnToRam/javaExternalCall/StringUtilImpl.java
@@ -4,4 +4,12 @@ public class StringUtilImpl {
 	public static String replaceFirst(String str,String regex, String replacement){
 		return str.replaceFirst(regex, replacement);
 	}
+	
+	public static String replaceAll(String str,String regex, String replacement){
+		return str.replaceAll(regex, replacement);
+	}
+	
+	public static String trim(String str){
+		return str.trim();
+	}
 }
